5120db3ea116a3ea3ce18db9814bdbc595329b18
Danielle Barros Bassetto <danibbassetto@hotmail.com>
2023-04-25 07:40:56 -0300

    new

--- src/Filme.java
+++ src/Filme.java
@@ -3,9 +3,19 @@ public class Filme
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
 
+    void exibeFichaTecnica()
+    {
+        System.out.println("Nome do Filme: " + this.nome);
+        System.out.println("Ano de lançamento: " + this.anoDeLancamento);
+    }
 
+    void avalia(double nota)
+    {
+        this.somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
 }
